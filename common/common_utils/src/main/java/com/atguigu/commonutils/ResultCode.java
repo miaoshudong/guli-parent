000cdb8072d179c01a84e2dd226f92e80a3a313f
miaoshudong <793126789@qq.com>
2022-08-02 12:07:57 +0800

    3c

---
+++ common/common_utils/src/main/java/com/atguigu/commonutils/ResultCode.java
@@ -0,0 +1,10 @@
+package com.atguigu.commonutils;
+
+/**
+ * @author miaoshudong
+ * @since 2022/8/2 11:02
+ */
+public interface ResultCode {
+    public static Integer SUCCESS = 20000;
+    public static Integer ERROR = 20001;
+}
--- service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -1,14 +1,20 @@
 package com.atguigu.eduservice.controller;
 
+import com.atguigu.commonutils.R;
 import com.atguigu.eduservice.entity.EduTeacher;
+import com.atguigu.eduservice.entity.vo.TeacherQuery;
 import com.atguigu.eduservice.service.EduTeacherService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
-
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/eduservice/teacher")
@@ -16,10 +22,74 @@ public class EduTeacherController {
     @Autowired
     private EduTeacherService teacherService;
 
-    @GetMapping("findAll")
-    public List<EduTeacher> findAllTeacher() {
+    //查询所有
+    @ApiOperation(value = "所有讲师列表")
+    @GetMapping("/findAll")
+    public R findAllTeacher() {
         List<EduTeacher> list = teacherService.list(null);
-        return list;
+        return R.ok().data("items", list);
+    }
+
+    //    逻辑删除讲师
+    @ApiOperation(value = "根据id删除讲师")
+    @DeleteMapping("{id}")
+    public R removeTeacher(@ApiParam(name = "id", value = "讲师ID", required = true)
+                           @PathVariable String id) {
+        boolean flag = teacherService.removeById(id);
+        if (flag) {
+            return R.ok();
+        } else {
+            return R.error();
+        }
+    }
+
+    // 分页查询
+    @GetMapping("pageTeacher/{current}/{limit}")
+    public R pageListTeacher(@PathVariable long current,
+                             @PathVariable long limit) {
+        Page<EduTeacher> pageTeacher = new Page<>(current, limit);
+        teacherService.page(pageTeacher, null);
+        //总记录数
+        long total = pageTeacher.getTotal();
+        //记录
+        List<EduTeacher> records = pageTeacher.getRecords();
+        Map map = new HashMap<>();
+        map.put("total", total);
+        map.put("rows", records);
+        return R.ok().data(map);
+    }
+
+    //条件查询 带分页
+    @PostMapping("pageTeacherCondition/{current}/{limit}")
+    public R pageTeacherCondition(@PathVariable long current,
+                                  @PathVariable long limit,
+                                  @RequestBody(required = false) TeacherQuery teacherQuery) {
+        Page<EduTeacher> pageTeacher = new Page<>(current, limit);
+        QueryWrapper<EduTeacher> wrapper = new QueryWrapper<>();
+        String name = teacherQuery.getName();
+        Integer level = teacherQuery.getLevel();
+        String begin = teacherQuery.getBegin();
+        String end = teacherQuery.getEnd();
+        if (!StringUtils.isEmpty(name)) {
+            wrapper.like("name", name);
+        }
+        if (!StringUtils.isEmpty(level)) {
+            wrapper.eq("level", level);
+        }
+        if (!StringUtils.isEmpty(begin)) {
+            wrapper.ge("gmt_create", begin);
+        }
+        if (!StringUtils.isEmpty(end)) {
+            wrapper.le("gmt_create", end);
+        }
+
+        teacherService.page(pageTeacher, wrapper);
+        long total = pageTeacher.getTotal();
+        List<EduTeacher> records = pageTeacher.getRecords();
+        Map map = new HashMap<>();
+        map.put("total", total);
+        map.put("rows", records);
+        return R.ok().data(map);
     }
 }
 
